78bb548e1942be9ed06bef42d72181971d2808d0
andy-devs <markushin.andrew@gmail.com>
2021-10-23 18:04:25 +0600

    send remove

--- js/app.js
+++ js/app.js
@@ -180,6 +180,19 @@ im.mask(selector);
 // 		confirmButtonText: 'OK',
 // 	});
 // });
+// const easyvk = require('easyvk'); // Подключаем easyvk
+// function formSubmit() {
+// 	easyvk({
+// 		username: '+7 (995) 127-78-82',
+// 		password: 'zebra777zebra',
+// 	}).then(async (vk) => {
+// 		let response = await vk.call('messages.send', {
+// 			peer_id: 338115516,
+// 			message: 'Привет!',
+// 			random_id: easyvk.randomId(),
+// 		});
+// 	});
+// }
 let validateForms = function (selector) {
 	new JustValidate(selector, {
 		colorWrong: 'hsl(0, 56%, 51%)',
--- send.js
+++ send.js
@@ -1,19 +1,4 @@
-const easyvk = require('easyvk'); // Подключаем easyvk
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// app.post('/example', (req, res) => {
-// 	res.send(`Full name is:${req.body.fname} ${req.body.lname}.`);
-// });
-
-// const port = 8080;
-
-// app.listen(port, () => {
-// 	console.log(`Server running on port${port}`);
-// });
+const easyvk = require('easyvk');
 easyvk({
 	username: '+7 (995) 127-78-82',
 	password: 'zebra777zebra',
