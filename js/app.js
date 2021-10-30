308cd0bdcb80a4cef1f84bb3ee28bba77d225c89
andy-devs <markushin.andrew@gmail.com>
2021-10-30 15:40:42 +0600

    form change

--- js/app.js
+++ js/app.js
@@ -236,7 +236,7 @@ window.mobileCheck = function () {
 	if (check === false) {
 		let form = document.querySelector('.form__body');
 		userDevice = 'pc';
-		form.action = 'https://formsubmit.co/nakodancestudio@gmail.com';
+		form.action = 'https://formsubmit.co/';
 	} else if (check === true) {
 		let form = document.querySelector('.form__body');
 		userDevice = 'mobile';
