0cb3c34de463a30b5eb4dd975beb9aa25cc37a08
andy-devs <markushin.andrew@gmail.com>
2022-02-16 17:36:53 +0600

    vk stats

--- index.php
+++ index.php
@@ -65,6 +65,24 @@
 				/></div
 		></noscript>
 		<!-- /Yandex.Metrika counter -->
+		<script type="text/javascript">
+			!(function () {
+				var t = document.createElement('script');
+				(t.type = 'text/javascript'),
+					(t.async = !0),
+					(t.src = 'https://vk.com/js/api/openapi.js?169'),
+					(t.onload = function () {
+						VK.Retargeting.Init('VK-RTRG-1206991-d4Crw'), VK.Retargeting.Hit();
+					}),
+					document.head.appendChild(t);
+			})();
+		</script>
+		<noscript
+			><img
+				src="https://vk.com/rtrg?p=VK-RTRG-1206991-d4Crw"
+				style="position: fixed; left: -999px"
+				alt=""
+		/></noscript>
 	</head>
 	<body>
 		<div class="first-wrapper">
