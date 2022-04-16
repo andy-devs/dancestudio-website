cc4dba17f3dfb0327373204da0b62ae455d2cf5d
andy-devs <markushin.andrew@gmail.com>
2022-04-16 11:34:34 +0600

    version update

--- index.php
+++ index.php
@@ -2582,6 +2582,6 @@
 			src="./libs/sweetalert2/dist/sweetalert2.all.min.js"
 		></script>
 
-		<script type="text/javascript" src="/js/newapp.js?v=1.9"></script>
+		<script type="text/javascript" src="/js/newapp.js?v=2.0"></script>
 	</body>
 </html>
